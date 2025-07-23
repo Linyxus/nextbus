7fabecfc42f66c42123e4374c9fb9b8fb3a5bfbb
Yichen Xu <yichen.x@outlook.com>
2025-07-24 01:05:44 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 from rich.logging import RichHandler
 from vendor.waveshare_epd import epd2in9b_V4
 from PIL import Image, ImageDraw, ImageFont
+from IPython import embed
 
 FORMAT = "%(message)s"
 logging.basicConfig(
@@ -22,6 +23,7 @@ def test1():
   bdraw.text((150, 30), "Bonjour!~~", font = font, fill = 0)
   bbuf = epd.getbuffer(black_image)
   rbuf = epd.getbuffer(red_image)
+  embed()
   # epd.init_Fast()
   epd.display_Fast(bbuf, rbuf)
 
@@ -37,15 +39,46 @@ def test2():
   # epd.init_Fast()
   epd.display_Partial(bbuf, 10, 60, 100, 120)
 
+def full_mask_of(num_bits: int) -> int:
+  if num_bits <= 0:
+    return 0
+  elif num_bits == 1:
+    return 1
+  else:
+    return full_mask_of(num_bits - 1) * 2 + 1
+
+def test3():
+  num_pixels = epd.height * epd.width
+  num_bytes = num_pixels // 8
+  def bbuf_at(i: int):
+    if i < num_bytes // 2:
+      return 255
+    else:
+      return 255
+  bbuf = [bbuf_at(i) for i in range(num_bytes)]
+  def rbuf_at(i: int):
+    return ~full_mask_of(i % 8)
+    # if i < num_bytes // 4:
+    #   if i % 8 == 1 or i % 8 == 4:
+    #     return 0
+    #   else:
+    #     return 255
+    # else:
+    #   return 255
+  rbuf = [rbuf_at(i) for i in range(num_bytes)]
+  epd.display_Fast(bbuf, rbuf)
+
 if __name__ == "__main__":
   log.info(f"Initializing EPD: height={epd.height}, width={epd.width}")
   epd.init_Fast()
   # epd.Clear_Fast()
 
-  input("Running test1... Press Enter to continue")
-  test1()
+  # input("Running test1... Press Enter to continue")
+  # test1()
   # input("Running test2... Press Enter to continue")
   # test2()
+  input("Running test3... Press Enter to continue")
+  test3()
 
   input("Clean and close ...")
   epd.Clear_Fast()
